49037ab41249c661c21490a4448e236b61f5ecff
brandib222 <brandi.danielle.ball@gmail.com>
2022-03-31 13:10:59 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/header';
+import Footer from './components/footer';
+import Projects from './components/projects';
+import Resume from './components/resume';
+
 function App() {
   return (
     <div className="App">
       <div>This is an App</div>
+      <Header />
+      <Projects />
+      <Resume />
+      <Footer />
     </div>
   );
 }
