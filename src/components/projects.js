73e5dd41a37e134ff8be6b538d37c50dcda8878e
brandib222 <brandi.danielle.ball@gmail.com>
2022-04-01 15:33:58 -0400

    nailed down the background

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,19 +5,18 @@ function Projects(){
     return (
         <div className='project-container'>
         <div className='projects'>
-            <h2> Welcome to my portfolio page </h2>
-            <p>Here are a few of my favorite projects</p>
+            <h2> Recent Projects </h2>
                 <a href='https://github.com/brandib222/secret-family-recipes'>
-                    <button className='recipes-app'> Secret Family Recipes
+                    <button className='recipes-app app-name'> Secret Family Recipes
                         <div className='tech-stack'>Tech Stack: Javascript, React.js, Node.js, Express, HTML, CSS
-                            <p>An app built with React that allows users to login, edit, update, and delete recipes</p>
+                            <p className='app-description'>An app built with React that allows users to login, edit, update, and delete recipes</p>
                         </div>
                     </button>
                 </a>
                 <a href='https://github.com/brandib222/nasa-photo-of-the-day'>
                     <button className='nasa-app'> NASA Photo of the Day
                         <div className='tech-stack'>Tech Stack: Javascript, React.js, Node.js, Express, HTML, CSS
-                            <p>An app that uses Axios to retrieve and display information from NASA's Photo of the Day API</p>
+                            <p className='app-description'>An app that uses Axios to retrieve and display information from NASA's Photo of the Day API</p>
                         </div>
                     </button>
                 </a>
@@ -25,7 +24,7 @@ function Projects(){
                 <a href='https://github.com/ft-anywhere-fitness-7/front-end'>
                     <button className='fitness-app'> Fitness App
                         <div className='tech-stack'>Tech Stack: Javascript, React.js, Node.js, Express, HTML, css
-                            <p>An app that allows users to register and login as either students or instructors</p>
+                            <p className='app-description'>An app that allows users to register and login as either students or instructors</p>
                         </div>
                     </button>
                     </a>
