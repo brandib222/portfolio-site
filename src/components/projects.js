c84828d9a36747870650fb9882d9ef9b51bfceb7
brandib222 <brandi.danielle.ball@gmail.com>
2022-04-01 13:44:31 -0400

    changed some styling

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,26 +5,30 @@ function Projects(){
     return (
         <div className='project-container'>
         <div className='projects'>
-        <h2> Projects </h2>
-        <a href='https://github.com/brandib222/secret-family-recipes'>
-            <button className='recipes-app'> Secret Family Recipes
-                <div className='tech-stack'>Tech Stack: Javascript, React.js, Node.js, Express, HTML, CSS</div>
-                <p>An app built with React that allows users to login, edit, update, and delete recipes</p>
-            </button>
-        </a>
-        <a href='https://github.com/brandib222/nasa-photo-of-the-day'>
-            <button className='nasa-app'> NASA Photo of the Day
-                <div className='tech-stack'>Tech Stack: Javascript, React.js, Node.js, Express, HTML, CSS</div>
-                <p>An app that uses Axios to retrieve and display information from NASA's Photo of the Day API</p>
-            </button>
-        </a>
+            <h2> Welcome to my portfolio page </h2>
+            <p>Here are a few of my favorite projects</p>
+                <a href='https://github.com/brandib222/secret-family-recipes'>
+                    <button className='recipes-app'> Secret Family Recipes
+                        <div className='tech-stack'>Tech Stack: Javascript, React.js, Node.js, Express, HTML, CSS
+                            <p>An app built with React that allows users to login, edit, update, and delete recipes</p>
+                        </div>
+                    </button>
+                </a>
+                <a href='https://github.com/brandib222/nasa-photo-of-the-day'>
+                    <button className='nasa-app'> NASA Photo of the Day
+                        <div className='tech-stack'>Tech Stack: Javascript, React.js, Node.js, Express, HTML, CSS
+                            <p>An app that uses Axios to retrieve and display information from NASA's Photo of the Day API</p>
+                        </div>
+                    </button>
+                </a>
 
-        <a href='https://github.com/ft-anywhere-fitness-7/front-end'>
-            <button className='fitness-app'> Fitness App
-                <div className='tech-stack'>Tech Stack: Javascript, React.js, Node.js, Express, HTML, CSS</div>
-                <p>An app that allows users to register and login as either students or instructors</p>
-            </button>
-            </a>
+                <a href='https://github.com/ft-anywhere-fitness-7/front-end'>
+                    <button className='fitness-app'> Fitness App
+                        <div className='tech-stack'>Tech Stack: Javascript, React.js, Node.js, Express, HTML, css
+                            <p>An app that allows users to register and login as either students or instructors</p>
+                        </div>
+                    </button>
+                    </a>
             </div>
         </div>
     )
--- src/components/resume.js
+++ src/components/resume.js
@@ -3,7 +3,6 @@ import '../App.css';
 function Resume(){
     return (
         <div className='resume-container'>
-            <h2>Resume</h2>
             <iframe title='resume' src="https://docs.google.com/document/d/e/2PACX-1vQfzIsz-Qo2L2hkV8OvGbQ9dgpA1j0q3VJy8Lsqet6IIM8vvqSzqMCidXyILcSPzjYzDQGjYHZoaWLp/pub?embedded=true" scrolling='no'></iframe>
         </div>
     )
