c95bf23ccd6796c1fb45903ce4c1440adaa209cf
brandib222 <brandi.danielle.ball@gmail.com>
2022-04-01 17:47:21 -0400

    mvp styling complete

--- src/components/resume.js
+++ src/components/resume.js
@@ -3,6 +3,7 @@ import '../App.css';
 function Resume(){
     return (
         <div className='resume-container'>
+            <h2 className='resume-header'> Resume and Work Experience</h2>
             <iframe title='resume' src="https://docs.google.com/document/d/e/2PACX-1vQfzIsz-Qo2L2hkV8OvGbQ9dgpA1j0q3VJy8Lsqet6IIM8vvqSzqMCidXyILcSPzjYzDQGjYHZoaWLp/pub?embedded=true"></iframe>
         </div>
     )
