46325c536cb8d44e4947fcdda46ff08c09bebf84
brandib222 <brandi.danielle.ball@gmail.com>
2022-04-04 19:08:07 -0400

    notes

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -9,6 +9,17 @@ function LandingPage(){
             <div className='right-side'>
             <div className='intro'>
                 <p className='skills'> Javascript React SQL Node.Js</p>
+                {/*<ul>
+                // LIST ALL OF MY SKILLS WHEN I HAVE TIME TO STYLE THEM
+                    <div className='frontend-skills'>Frontend:</div>
+                    <li> Javascript </li>
+                    <li> React </li>
+                    <li> Redux </li>
+                    <li> CSS </li>
+                    <li> Html </li>
+                    <li> Restful APIs </li>
+                    <li> Object Oriented Programming (OOP) </li>
+                </ul>*/}
                 <p> Welcome to the portfolio website of Brandi Ball! Stay informed, collaborate, and keep up with projects that I've been working on. Take a look at my work: </p>
             </div>
             <div className='landing-links'>
