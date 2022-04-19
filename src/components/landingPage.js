e02b32b0723b79372f40b972bd337defaf82685d
brandib222 <brandi.danielle.ball@gmail.com>
2022-04-19 18:41:36 -0400

    added skills section

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -8,26 +8,33 @@ function LandingPage(){
         <div className='landing-container'>
             <img src='https://avatars.githubusercontent.com/u/91443920?v=4' alt='profile pic'/>
             <div className='right-side'>
-            <div className='intro'>
-                <p className='skills'> Javascript React SQL Node.Js</p>
-                {/*<ul>
-                // LIST ALL OF MY SKILLS WHEN I HAVE TIME TO STYLE THEM
-                    <div className='frontend-skills'>Frontend:</div>
-                    <li> Javascript </li>
-                    <li> React </li>
-                    <li> Redux </li>
-                    <li> CSS </li>
-                    <li> Html </li>
-                    <li> Restful APIs </li>
-                    <li> Object Oriented Programming (OOP) </li>
-                </ul>*/}
-                <p> Welcome to the portfolio website of Brandi Ball! Stay informed, collaborate, and keep up with projects that I've been working on. Take a look at my work: </p>
+                <div className='intro'>
+                    <p className='skills'> Javascript React SQL Node.Js</p>
+                    <p> Welcome to the portfolio website of Brandi Ball! Stay informed, collaborate, and keep up with projects that I've been working on. Take a look at my work: </p>
+                </div>
+                <div className='landing-links'>
+                    <Link to='/projects'>Projects</Link>
+                    <Link to='/resume'>Resume</Link>
+                </div>
             </div>
-            <div className='landing-links'>
-                <Link to='/projects'>Projects</Link>
-                <Link to='/resume'>Resume</Link>
-            </div>
-            </div>
+            <ul className='all-skills-container'>
+                        <div className='frontend-skills'>Frontend:
+                            <p> Javascript </p>
+                            <p> React </p>
+                            <p> Redux </p>
+                            <p> CSS </p>
+                            <p> Html </p>
+                            <p> Restful APIs </p>
+                            <p> Axios </p>
+                        </div>
+                        <div className='backend-skills'>Backend:
+                            <p> SQL </p>
+                            <p> PostgreSQL </p>
+                            <p> Node.js </p>
+                            <p> Express </p>
+                            <p> Python </p>
+                        </div>
+                    </ul>
         </div>
     )
 }
